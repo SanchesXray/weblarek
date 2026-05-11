4f2831fec925307b1a7fcda9047e5c2e00a880e6
SanchesXray <sanches.xray@yandex.ru>
2026-05-12 02:30:05 +0300

    Weblarek PR9 Done2 корректировка Basket.ts

--- src/components/view/Basket.ts
+++ src/components/view/Basket.ts
@@ -6,7 +6,6 @@ export class Basket extends Component<any> {
     private listContainer: HTMLElement | null;
     private buttonElement: HTMLButtonElement | null;
     private priceElement: HTMLElement | null;
-    private emptyMessage: HTMLElement;
     private eventBus: EventEmitter;
 
     constructor(container: HTMLElement, eventBus: EventEmitter) {
@@ -15,11 +14,6 @@ export class Basket extends Component<any> {
         this.listContainer = container.querySelector('.basket__list');
         this.buttonElement = container.querySelector('.basket__button');
         this.priceElement = container.querySelector('.basket__price');
-
-        this.emptyMessage = document.createElement('p');
-        this.emptyMessage.textContent = 'Корзина пуста';
-        this.emptyMessage.style.textAlign = 'center';
-        this.emptyMessage.style.padding = '20px';
 
         this.buttonElement?.addEventListener('click', () => {
             this.eventBus.emit('basket:create-order');
@@ -32,7 +26,6 @@ export class Basket extends Component<any> {
         this.listContainer.innerHTML = '';
 
         if (cards.length === 0) {
-            this.listContainer.appendChild(this.emptyMessage);
         } else {
             cards.forEach(card => {
                 this.listContainer?.appendChild(card.render());
